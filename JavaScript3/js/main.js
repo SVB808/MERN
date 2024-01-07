a82e49b9067b9a4cd2ea69482194846425e12254
SVB808 <shruthavbhat@gmail.com>
2024-01-07 12:34:34 +0530

    Basics of JavaScript

---
+++ JavaScript3/js/main.js
@@ -0,0 +1,16 @@
+//Strings
+const myVariable = "Mathematics";
+
+//The length property
+console.log(myVariable.length);
+console.log("Every good boy does fine".length);
+
+
+//String Methods
+console.log(myVariable.charAt(10));
+console.log(myVariable.indexOf("at"));
+console.log(myVariable.lastIndexOf("at"));
+console.log(myVariable.slice(5,-1));
+console.log(myVariable.toUpperCase());
+console.log(myVariable.toLowerCase());
+
