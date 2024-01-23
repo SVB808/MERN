0b9994ccc1bad40aaab10bec77647a39ad54040c
SVB808 <shruthavbhat@gmail.com>
2024-01-23 19:31:14 +0530

    .

--- JavaScript3/js/main.js
+++ JavaScript3/js/main.js
@@ -1,5 +1,5 @@
 //Strings
-const myVariable = "Mathematics";
+const myVariable = "Mathematics is fun";
 
 //The length property
 console.log(myVariable.length);
@@ -11,6 +11,15 @@ console.log(myVariable.charAt(10));
 console.log(myVariable.indexOf("at"));
 console.log(myVariable.lastIndexOf("at"));
 console.log(myVariable.slice(5,-1));
+console.log(myVariable.slice(5));
 console.log(myVariable.toUpperCase());
 console.log(myVariable.toLowerCase());
+console.log(myVariable.includes("div"));
+console.log(myVariable.includes("matic"));
+console.log(myVariable.split("e"));
+console.log(myVariable.split(""));
+console.log(myVariable.split(" "));
+console.log(myVariable.split("i"));
+console.log("John,Jade,Joe".split(","));
+console.log("John Jade Joe".split(" "));
 
